75e3464e3f9df42b84bdb042d29259ba3e8f5a5a
Lizzie Szoke <lizzieszoke@Lizzies-MacBook-Air.local>
2016-07-11 13:32:01 -0600

    Added more content

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ app.controller("MyController", ['$scope', function($scope){
   $scope.view = {};
   $scope.view.portfolio = false;
   $scope.view.experience = false;
+  $scope.view.education = false;
+  $scope.view.skills = false;
+  $scope.view.about = false;
 }]);
